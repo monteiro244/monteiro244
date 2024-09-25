caa924c5e82bf5a25d728fd9706535678952f50e
monteiro244 <jv8515409@gmail.com>
2024-09-25 11:05:48 -0300

    criei o repositorio

---
+++ aula.js
@@ -0,0 +1,12 @@
+var prompt = require('prompt-sync')();
+
+let atendimento1 = prompt("oi, tudo bem? O que vc deseja?")
+
+console.log('oi, tudo sim! Estou procurando uma peça de roupa para a minha filha.')
+
+let atendimento2= prompt("hmmm, que gal! E vc ja sabe qual tipo de peça vai querer?")
+
+console.log('sim,sim! Vou escolher uma blusa pra ela.')
+
+let atendimento3= prompt("temos essas daqui, pode dar uma olhadinha e ver qual vc acha que combinaria maid coma a sua filha")
+
